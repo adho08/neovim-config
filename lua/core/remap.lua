747f6bf665b00fc610293ecd37c2cd156fcd8a8d
Adrian Hossner <adhos@AdriansLaptop.localdomain>
2025-08-09 08:57:47 +0200

    configurating

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,14 +1,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', "<leader>w", vim.cmd.w) -- write
-vim.keymap.set('n', "<leader>q", vim.cmd.q) -- quit
-vim.keymap.set('n', "<leader>sr", ":%s/") --search and replace
-vim.keymap.set('v', "<leader>sr", ":s/")
-vim.keymap.set('n', "<leader>v", "<C-v>") -- visual block mode
+vim.keymap.set('n', "<leader>w", vim.cmd.w, { desc = "write" })
+vim.keymap.set('n', "<leader>q", vim.cmd.q, { desc = "quit" })
+vim.keymap.set('n', "<leader>sr", ":%s/", { desc = "search and replace" })
+vim.keymap.set('v', "<leader>sr", ":s/", { desc = "search and replace" })
+vim.keymap.set('n', "<leader>v", "<C-v>", { desc = "visual block mode" })
+vim.keymap.set('n', "<C-k>", "O<esc>j", { desc = "append empty line above" })
+vim.keymap.set('n', "<C-j>", "o<esc>k", { desc = "append empty line below" })
+vim.keymap.set('i', "<C-k>", "<esc>Oj", { desc = "append empty line above" })
+vim.keymap.set('i', "<C-j>", "<esc>ok", { desc = "append empty line below" })
+vim.keymap.set('n', "o", "A<CR><esc>", { desc = "append empty line below and jump" })
+vim.keymap.set('n', "O", "kA<CR><esc>", { desc = "append empty line below and jump" })
 
--- Open Vifm
-vim.keymap.set('n', "<leader>fm", ":Vifm<cr>")
+vim.keymap.set('n', "<leader>fm", ":Vifm<cr>", { desc = "Open Vifm" })
 
 vim.keymap.set('v', '<C-a>', ':s/\\%V\\d\\+\\%V/\\=submatch(0)+1/g<CR>', { desc = 'Increment numbers in selection' })
 vim.keymap.set('v', '<C-x>', ':s/\\%V\\d\\+\\%V/\\=submatch(0)-1/g<CR>', { desc = 'Decrement numbers in selection' })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
 			-- key mappings
 			vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)  -- go to definition
 			vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts) -- go to declaration
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)        -- hover info
+			vim.keymap.set('n', 'B', vim.lsp.buf.hover, opts)        -- hover info
 			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- go to implementation
 			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- rename symbol
 			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts) -- code actions
